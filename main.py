06b0241da4c873ee44fd3f03ae90df4945a3c8b8
cinntiacosta <150087373+cinntiacosta@users.noreply.github.com>
2023-11-13 21:37:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+# declaração de variáveis
+
+nome = "Cintia"
+idade = 28
+altura = 1.70
+peso = 54
+
+print("Olá, pessoal!")
+
+# imprimir na tela o valor das variáveis
+print(nome) 
+print(idade)
+print(altura)
+print(peso)
+
+print("Me chamo", nome, "tenho", idade, "anos", "Minha altura é", altura, "m e peso", peso, "kg.")
+
+print(type(nome)) 
+print(type(idade))
+print(type(altura))
+print(type(peso))
+
+aluno = input("Digite o nome do aluno: ")
+print("O aluno é o", aluno)
+
+print("-------------------")
+
+Nome = input("Qual o seu nome: ")
+print("Olá,", Nome, "! Seja bem-vindo ao Letramento Digital!")
+
+print(type(Nome))
+
+
+print("-------------------")
+
+seu_nome = input("Digite seu nome: ")
+nome_amigo = input("Digite a idade do seu melhor amigo: ")
+idade_amigo = input("Digite a idade do seu melhor amigo:")
+
+mensagem = "Olá, " + seu_nome + "! Seu melhor amigo tem " + idade_amigo + " anos de idade " + "e o nome dele é " + nome_amigo + "."
+
+
+print(mensagem)
